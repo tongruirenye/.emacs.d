27d8a077f38aac3a2593ffe1b75543d1b475738c
tongruirenye <tongruirenye@163.com>
2020-04-10 09:24:55 +0800

    modify org conf

--- custom/setup-org.el
+++ custom/setup-org.el
@@ -35,6 +35,7 @@
 (require 'org-journal)
 (customize-set-variable 'org-journal-dir (concat org-directory "journal/"))
 (setq org-journal-file-type 'daily)
+(setq org-journal-enable-agenda-integration t)
 (defun org-journal-find-location ()
   ;; Open today's journal, but specify a non-nil prefix argument in order to
   ;; inhibit inserting the heading; org-capture will insert the heading.
@@ -47,6 +48,12 @@
 ;; capture
 (setq org-capture-templates '(("j" "Journal entry" entry (function org-journal-find-location)
                                "** %(format-time-string org-journal-time-format)%^{Title}\n%i%?")
+                               ("n" "Note entry" entry (function org-journal-find-location)
+                               "** NOTE %(format-time-string org-journal-time-format)%^{Title}\n    %i%?")
+                              ("e" "Event entry" entry (function org-journal-find-location)
+                               "** EVENT %(format-time-string org-journal-time-format)%^{Title}\n    %i%?")
+                              ("t" "Task entry" entry (function org-journal-find-location)
+                               "** TODO %(format-time-string org-journal-time-format)%^{Title}\n    %i%?")
                               ("c" "web capture" entry (function org-journal-find-location)
                                "** %(format-time-string org-journal-time-format)%:description\n   [[%:link][%:description]]\n   %:initial\n   " :immediate-finish t)
                               ))
@@ -92,42 +99,106 @@
 
 ;; pomodoro
 (require 'org-pomodoro)
-
 (defun my-org-pomodoro-notify (orig-fun &rest args)
   "Send a toast notification on windows 10"
   (apply orig-fun args)
   (call-process-shell-command (apply #'format "powershell -ExecutionPolicy Bypass -File %s %s %s" my-windows-toast-file args) nil "*scratch*")
-  (org-save-all-org-buffers)
   )
 (advice-add 'org-pomodoro-notify :around #'my-org-pomodoro-notify)
-
 
 (defun my-org-notify_handler (msg)
   (let ((m (list "Effort" msg)))
     (call-process-shell-command (format "powershell -ExecutionPolicy Bypass -File %s %s %s" my-windows-toast-file "Effort" msg) nil "*scratch*")))
 (setq org-show-notification-handler 'my-org-notify_handler)
 
-(defun my-start-pomodoro ()
-  (interactive)
+(defun my-clock-in-hook ()
   (let* ((org-journal-find-file 'find-file)
-        (cc-todo-list '())
-        (cc-todo-pos '())
-        (prev-buffer (current-buffer)))
+         (id (org-entry-get (point) "ID"))
+         (content (nth 4 (org-heading-components)))
+         (prev-buffer (current-buffer)))
+    (save-excursion
+      (save-restriction
+        (org-journal-new-entry t)
+        (org-insert-heading '(16))
+        (insert "TASK ")
+        (insert (format-time-string "%H:%M " org-clock-start-time))
+        (insert (format "[[id:%s][%s]]" id content))
+        (org-clock-find-position nil)
+        (insert-before-markers "\n")
+	    (backward-char 1)
+	    (org-indent-line)
+	    (insert org-clock-string " ")
+        (org-insert-time-stamp org-clock-start-time
+					   'with-hm 'inactive)
+        ))
+    (switch-to-buffer prev-buffer)
+    (org-save-all-org-buffers)))
+
+(add-hook 'org-clock-in-hook 'my-clock-in-hook)
+
+(defun my-clock-out-hook ()
+  (let* ((org-journal-find-file 'find-file)
+         (id (org-entry-get (point) "ID"))
+         (content (nth 4 (org-heading-components)))
+         (time (format-time-string "%H:%M " org-clock-start-time))
+         (title (concat "** TASK " time (format "\\[\\[id:%s\\]\\[%s\\]\\]" id content)))
+         (prev-buffer (current-buffer))
+         )
     (save-excursion
       (save-restriction
         (org-journal-new-entry t)
         (goto-char (point-min))
         (org-map-entries (lambda ()
-                           (push (nth 4 (org-heading-components)) cc-todo-list)
-                           (push (cons (nth 4 (org-heading-components)) (point))
-                                 cc-todo-pos)) "TODO=\"INPROGRESS\"")
-        (ivy-read "Peek a Item:" cc-todo-list
-                  :action (lambda (x)
-                            (dolist (v cc-todo-pos)
-                              (if (string-equal x (car v))
-                                  (goto-char (cdr v))))
-                            (org-pomodoro)))))
-    (switch-to-buffer prev-buffer)))
+                           (if (looking-at title)
+                               (let (ts
+                                     te
+                                     s
+                                     h
+                                     m)
+                                 (org-clock-find-position t)
+                                 (if (and (looking-at (concat "[ \t]*" org-keyword-time-regexp))
+                                          (equal (match-string 1) org-clock-string))
+                                     (setq ts (match-string 2))
+                                   (if fail-quietly (throw 'exit nil) (error "Clock start time is gone")))
+                                 (goto-char (match-end 0))
+                                 (delete-region (point) (point-at-eol))
+                                 (insert "--")
+                                 (setq te (org-insert-time-stamp org-clock-out-time 'with-hm 'inactive))
+                                 (setq s (org-time-convert-to-integer
+                                          (time-subtract
+                                           (org-time-string-to-time te)
+                                           (org-time-string-to-time ts)))
+                                       h (floor s 3600)
+                                       m (floor (mod s 3600) 60))
+                                 (insert " => " (format "%2d:%02d" h m))
+                                 ))
+                           ) "TODO=\"TASK\"")
+        ))
+    (switch-to-buffer prev-buffer)
+    (org-save-all-org-buffers)
+    ))
+
+(add-hook 'org-clock-out-hook 'my-clock-out-hook)
+
+(defun my-org-todo-trigger-hook (list)
+  (if (equal "DONE" (plist-get list :to))
+      (let* ((org-journal-find-file 'find-file)
+             (id (org-entry-get (point) "ID"))
+             (content (nth 4 (org-heading-components)))
+             (time (format-time-string "%H:%M "))
+             )
+        (save-excursion
+          (save-restriction
+            (org-journal-new-entry t)
+            (org-insert-heading '(16))
+            (insert "DONE ")
+            (insert time)
+            (insert (format "[[id:%s][%s]]" id content))
+            ))
+        (org-save-all-org-buffers))))
+
+(add-hook 'org-trigger-hook 'my-org-todo-trigger-hook)
+
 
 
 (global-set-key "\C-ca" 'org-agenda)
--- init.el
+++ init.el
@@ -15,15 +15,19 @@
 (require 'setup-ivy)
 (require 'setup-programming)
 (require 'setup-org)
+(require 'setup-window)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-modules
+   (quote
+    (org-protocol org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m)))
  '(package-selected-packages
    (quote
-    (company-go go-mode org-super-agenda htmlize neotree company-web  company web-mode js2-mode doom-themes projectile which-key counsel))))
+    (org-drill request lsp-ui flycheck company-lsp lsp-mode company-ctags company-go go-mode org-super-agenda htmlize neotree company-web company web-mode js2-mode doom-themes projectile which-key counsel))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
