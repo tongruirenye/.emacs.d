75dbeb1788faf6f58506658aee03e48ba5e9f8ca
胡红 <>
2022-03-20 20:49:06 +0800

    add

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -3,11 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(elfeed-curl-extra-arguments '("-k"))
  '(org-icalendar-alarm-time 10)
  '(org-modules
    '(ol-bbdb ol-bibtex ol-docview ol-eww ol-gnus org-habit ol-info ol-irc ol-mhe org-protocol ol-rmail ol-w3m))
  '(package-selected-packages
-   '(modern-cpp-font-lock citre company-quickhelp treemacs emacsql-sqlite3 org-roam ace-window pretty-hydra company counsel all-the-icons find-file-in-project doom-modeline mwim ox-hugo yankpad org-edna shackle calfw-org calfw org-clock org-protocol avy which-key web-mode use-package simple-modeline request projectile popup org-super-agenda org-pomodoro org-drill org-brain neotree lsp-ui js2-refactor htmlize flycheck doom-themes dashboard counsel-etags company-web company-lsp company-go company-ctags)))
+   '(dirvish elfeed-org elfeed modern-cpp-font-lock citre company-quickhelp treemacs emacsql-sqlite3 org-roam ace-window pretty-hydra company counsel all-the-icons find-file-in-project doom-modeline mwim ox-hugo yankpad org-edna shackle calfw-org calfw org-clock org-protocol avy which-key web-mode use-package simple-modeline request projectile popup org-super-agenda org-pomodoro org-drill org-brain neotree lsp-ui js2-refactor htmlize flycheck doom-themes dashboard counsel-etags company-web company-lsp company-go company-ctags)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
